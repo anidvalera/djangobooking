82a7e091baf195a35436ab4a9f4f1fa55a619f05
anidvalera <davqac509@gmail.com>
2026-06-13 18:22:45 +0300

    Update wsgi.py

--- smth/booking/booking_system/wsgi.py
+++ smth/booking/booking_system/wsgi.py
@@ -1,5 +1,11 @@
 import os
-from django.core.wsgi import get_wsgi_application
+import sys
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'booking_system.settings')
+path = '/home/yourusername/booking2/booking_project/booking'
+if path not in sys.path:
+    sys.path.append(path)
+
+os.environ['DJANGO_SETTINGS_MODULE'] = 'booking_system.settings'
+
+from django.core.wsgi import get_wsgi_application
 application = get_wsgi_application()
