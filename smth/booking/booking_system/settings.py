0a77d21999ed53c8df901a3442f7ddd42850e9cd
anidvalera <davqac509@gmail.com>
2026-06-13 17:45:01 +0300

    .

--- smth/booking/booking_system/settings.py
+++ smth/booking/booking_system/settings.py
@@ -64,11 +64,7 @@ USE_TZ = True
 STATIC_URL = 'static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-# --- Email (підтвердження бронювань) ---
-# DEV: листи друкуються в консоль. Для продакшну замініть на SMTP-бекенд:
-#   EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-#   EMAIL_HOST = 'smtp.example.com'; EMAIL_PORT = 587; EMAIL_USE_TLS = True
-#   EMAIL_HOST_USER = '...'; EMAIL_HOST_PASSWORD = '...'
+
 EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 DEFAULT_FROM_EMAIL = 'Система бронювання <noreply@booking.local>'
 
